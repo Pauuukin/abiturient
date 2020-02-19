0c718d9502c36a152c68d93035d19686d36aa751
Pauuukin <oleg.paukin@mail.ru>
2020-02-19 11:09:22 +0300

    added the abitur app and started doing routing

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,4 +1,4 @@
-"""abitur URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+# from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('abitur/', include('abitur.urls'))
 ]
