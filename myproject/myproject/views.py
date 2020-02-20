e8348267047eea42d245cd64d6e806d37790c0a8
Pauuukin <oleg.paukin@mail.ru>
2020-02-20 11:37:50 +0300

    make main page

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def mainPage(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'index_base.html')
