5c2a5a3cebf09dcd88df38049581dcb818b99160
Pauuukin <oleg.paukin@mail.ru>
2020-02-28 12:50:36 +0300

    add model for news, share admin panel and change media setting

--- myproject/abitur/models.py
+++ myproject/abitur/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 
 
+class News (models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    body = models.TextField()
+    img = models.ImageField(upload_to='pictures/%Y/%m/', max_length=255)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
--- myproject/abitur/views.py
+++ myproject/abitur/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def abiturPage(request):
-    n = ['Oleg', 'igor', 'inna', 'artem', 'nastya']
-    return render(request, 'abitur/index.html', context={'names': n})
+    news = News.objects.all()
+    return render(request, 'abitur/index.html', context={'news': news})
 
 def bakPage(request):
     return HttpResponse('<h1>Bak page</h1>')
