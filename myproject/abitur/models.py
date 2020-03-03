4d040d17d1dbb9a6c1633c8723547aea083c0402
Pauuukin <oleg.paukin@mail.ru>
2020-03-03 16:22:04 +0300

    main page done

--- myproject/abitur/models.py
+++ myproject/abitur/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class News (models.Model):
     title = models.CharField(max_length=200, db_index=True)
     body = models.TextField()
-    img = models.ImageField(upload_to='pictures/%Y/%m/', max_length=255)
+    img = models.ImageField(upload_to='pictures', max_length=255)
     date_pub = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
 
